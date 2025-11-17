b71db90b6f70bed9e31e9104acd471cea79f83c6
zhangjian2661 <14090993+zhangjian2661@user.noreply.gitee.com>
2025-11-16 21:25:51 -0500

    begin

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by 张建 on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
