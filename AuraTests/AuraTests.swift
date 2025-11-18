7778eb1a346927bae32f598a9d0b4b95dbd7d3e4
zhangjian2661 <14090993+zhangjian2661@user.noreply.gitee.com>
2025-11-17 20:08:49 -0500

    init

---
+++ AuraTests/AuraTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuraTests.swift
+//  AuraTests
+//
+//  Created by 张建 on 2025/11/17.
+//
+
+import Testing
+@testable import Aura
+
+struct AuraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
